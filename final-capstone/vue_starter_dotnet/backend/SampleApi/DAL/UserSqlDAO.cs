65a26b577c2dcc75e124e7593dca7bdb04252ef3
Bradley <bradleyhills@gmail.com>
2019-12-27 14:58:32 -0500

    Registering User works with Postman

---
+++ final-capstone/vue_starter_dotnet/backend/SampleApi/DAL/UserSqlDAO.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Npgsql;
+using SampleApi.Models;
+
+namespace SampleApi.DAL
+{
+    /// <summary>
+    /// A SQL Dao for user objects.
+    /// </summary>
+    public class UserSqlDAO : IUserDAO
+    {
+        private readonly string connectionString;
+
+        /// <summary>
+        /// Creates a new sql dao for user objects.
+        /// </summary>
+        /// <param name="connectionString">the database connection string</param>
+        public UserSqlDAO(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        /// <summary>
+        /// Saves the user to the database.
+        /// </summary>
+        /// <param name="user"></param>
+        public void CreateUser(User user)
+        {
+            try
+            {
+                using (NpgsqlConnection conn = new NpgsqlConnection(connectionString))
+                {
+                    conn.Open();
+                    NpgsqlCommand cmd = new NpgsqlCommand("insert into users (username, password, salt, role) values( @username, @password, @salt, @role)", conn);
+                    cmd.Parameters.AddWithValue("@username", user.Username);
+                    cmd.Parameters.AddWithValue("@password", user.Password);
+                    cmd.Parameters.AddWithValue("@salt", user.Salt);
+                    cmd.Parameters.AddWithValue("@role", user.Role);
+
+                    cmd.ExecuteNonQuery();
+
+                    return;
+                }
+            }
+            catch(NpgsqlException ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// Deletes the user from the database.
+        /// </summary>
+        /// <param name="user"></param>
+        public void DeleteUser(User user)
+        {
+            try
+            {
+                using (NpgsqlConnection conn = new NpgsqlConnection(connectionString))
+                {
+                    conn.Open();
+                    NpgsqlCommand cmd = new NpgsqlCommand("DELETE FROM users WHERE id = @id;", conn);
+                    cmd.Parameters.AddWithValue("@id", user.Id);                    
+
+                    cmd.ExecuteNonQuery();
+
+                    return;
+                }
+            }
+            catch (NpgsqlException ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// Gets the user from the database.
+        /// </summary>
+        /// <param name="username"></param>
+        /// <returns></returns>
+        public User GetUser(string username)
+        {
+            User user = null;
+            try
+            {
+                using (NpgsqlConnection conn = new NpgsqlConnection(connectionString))
+                {
+                    conn.Open();
+                    NpgsqlCommand cmd = new NpgsqlCommand("SELECT * FROM USERS WHERE username = @username;", conn);
+                    cmd.Parameters.AddWithValue("@username", username);
+
+                    NpgsqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        user = MapRowToUser(reader);
+                    }
+                }
+
+                return user;
+            }
+            catch (NpgsqlException ex)
+            {
+                throw ex;
+            }            
+        }
+
+        /// <summary>
+        /// Updates the user in the database.
+        /// </summary>
+        /// <param name="user"></param>
+        public void UpdateUser(User user)
+        {
+            try
+            {
+                using (NpgsqlConnection conn = new NpgsqlConnection(connectionString))
+                {
+                    conn.Open();
+                    NpgsqlCommand cmd = new NpgsqlCommand("UPDATE users SET password = @password, salt = @salt, role = @role WHERE id = @id;", conn);                    
+                    cmd.Parameters.AddWithValue("@password", user.Password);
+                    cmd.Parameters.AddWithValue("@salt", user.Salt);
+                    cmd.Parameters.AddWithValue("@role", user.Role);
+                    cmd.Parameters.AddWithValue("@id", user.Id);
+
+                    cmd.ExecuteNonQuery();
+
+                    return;
+                }
+            }
+            catch (NpgsqlException ex)
+            {
+                throw ex;
+            }
+        }
+
+        private User MapRowToUser(NpgsqlDataReader reader)
+        {
+            return new User()
+            {
+                Id = Convert.ToInt32(reader["id"]),
+                Username = Convert.ToString(reader["username"]),
+                Password = Convert.ToString(reader["password"]),
+                Salt = Convert.ToString(reader["salt"]),
+                Role = Convert.ToString(reader["role"])
+            };
+        }
+    }
+}
